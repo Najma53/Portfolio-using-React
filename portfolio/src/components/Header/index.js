ccf43fe5b633480a3f2108de48ab8809167ea6f0
Najma53 <najma.alibhai@gmail.com>
2023-02-13 10:23:02 +0000

    was getting component/Header not found error when npm start was done, found out u need to name index.js file and not header.js , still nav bar not seen on screen but no error

---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./style.css";
+import { NavLink } from "react-router-dom";
+
+
+function Header() {
+    return (
+<ul className="nav nav-tabs navbar navbar-expand-lg navbar-light bg-light">
+      <li className="nav-item">
+            <NavLink
+                to="/"
+                end
+                className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+                }
+        >        Home
+            </NavLink>
+        </li>
+
+
+      <li className="nav-item">
+            <NavLink
+                to="about"
+                className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+                }
+        >       About
+            </NavLink>
+      </li>
+
+      <li className="nav-item">
+            <NavLink
+                to="gallery"
+                className={({ isActive }) =>
+                 isActive ? "nav-link active" : "nav-link"
+          }
+        >        Gallery
+            </NavLink>
+      </li>
+
+      <li className="nav-item">
+            <NavLink
+                to="contact"
+                end
+                className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+            }
+        >   Contact
+            </NavLink>
+      </li>
+    </ul>
+
+    );
+
+    }
+
+export default Header;
