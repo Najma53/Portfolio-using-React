7ffa02f94ff2aa8fc177f03a40f1ee9d8ed82d5f
Najma53 <najma.alibhai@gmail.com>
2023-02-16 09:45:31 +0000

    updated index.js in Contact Folder plus a bit of styling and updated new images in folder

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -12,15 +12,21 @@ function Contact() {
     
 
                 <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
-                    <li>
-                        <a href="mailto:najma.alibhai@gmail.com">E-mail</a>
-                    </li>
+                    <div className="col-4">
+                        <a href="mailto:najma.alibhai@gmail.com"><img src={process.env.PUBLIC_URL + "/image/email.png"}></img></a>
+                    </div>
                 </div>
 
                 <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
-                    <li>
-                        <a href="https://github.com//Najma53/">GitHub</a>
-                    </li>
+                    <div className="col-4">
+                        <a href="https://github.com//Najma53/"><img src={process.env.PUBLIC_URL + "/image/github.png"}></img></a>
+                    </div>
+                </div>
+
+                <div className="cardContainer col-lg-4 col-md-4 col-sm-12">
+                    <div className="col-4">
+                        <a href="www.linkedin.com/in/najma53"><img src={process.env.PUBLIC_URL + "/image/linkedin.png"}></img></a>
+                    </div>
                 </div>
             </div>
             </div>
